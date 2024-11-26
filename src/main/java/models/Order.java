59917e6cf3c6db3d7b142f12c27f83366e597daa
ledkaa <ledkaa@gmai.com>
2024-11-26 14:24:21 +0000

    Order

---
+++ src/main/java/models/Order.java
@@ -0,0 +1,16 @@
+package models;
+
+    import java.util.Date;
+import java.util.List;
+
+    public class Order {
+        private Long orderId;
+        private Long userId;
+        private List<Appliance> appliances;
+        private Date orderDate;
+        private String status;
+        private double totalAmount;
+
+        // Constructors, Getters, and Setters
+    }
+
